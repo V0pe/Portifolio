b57b7bda66a5ac5bd6793e32eb9c32e3a782853e
iBel251 <ibbimran7@gmail.com>
2022-04-04 19:33:07 +0300

    Add mobile menu options functionality

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const menuOpen = document.getElementById('menu');
-const navbar = document.querySelector('.navbar');
-const menuClose = document.querySelector('.fa-times');
+let menuOpen = document.getElementById("menu");
+let navbar = document.querySelector(".navbar");
+let menuClose = document.querySelector(".fa-times");
+let menuList = document.querySelectorAll(".navbars a");
 
-menuOpen.addEventListener('click', () => {
-  navbar.style.top = '0';
-});
 
-menuClose.addEventListener('click', () => {
-  navbar.style.top = '-98vh';
-});
+menuOpen.addEventListener("click", () => { 
+    navbar.style.top = "0"})
+
+menuClose.addEventListener("click", () => { 
+    navbar.style.top = "-98vh"})
+
+for (let i=0;i<3;i++){
+  menuList[i].addEventListener("click", ()=> {
+    navbar.style.top = "-98vh";})};
