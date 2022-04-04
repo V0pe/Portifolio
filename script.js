2cc5d06734907c70ae26fbf047aa5671e5081e60
iBel251 <ibbimran7@gmail.com>
2022-04-04 21:31:39 +0300

    Fix ESLint errors

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-let menuOpen = document.getElementById("menu");
-let navbar = document.querySelector(".navbar");
-let menuClose = document.querySelector(".fa-times");
-let menuList = document.querySelectorAll(".navbars a");
+const menuOpen = document.getElementById('menu');
+const navbar = document.querySelector('.navbar');
+const menuClose = document.querySelector('.fa-times');
+const menuList = document.querySelectorAll('.navbars a');
 
+menuOpen.addEventListener('click', () => {
+  navbar.style.top = "0"
+})
 
-menuOpen.addEventListener("click", () => { 
-    navbar.style.top = "0"})
+menuClose.addEventListener('click', () => {
+  navbar.style.top = "-98vh"
+});
 
-menuClose.addEventListener("click", () => { 
-    navbar.style.top = "-98vh"})
-
-for (let i=0;i<3;i++){
-  menuList[i].addEventListener("click", ()=> {
-    navbar.style.top = "-98vh";})};
+for (let i = 0; i < 3; i++) {
+  menuList[i].addEventListener('click', () => {
+    navbar.style.top = "-98vh";
+  })
+}
