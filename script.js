1ce98defba1dacdc7b406a3e023b1acf690d6617
ARANY <v.aderibigbe96@gmail.com>
2022-04-05 02:49:02 -0400

    add script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const menuOpen = document.getElementById('menu');
-const navbar = document.querySelector('.navbar');
-const menuClose = document.querySelector('.fa-times');
 
-menuOpen.addEventListener('click', () => {
-  navbar.style.top = '0';
-});
+let menuOpen = document.getElementById("menu");
+let navbar = document.querySelector(".navbar");
+let menuClose = document.querySelector(".fa-times");
 
-menuClose.addEventListener('click', () => {
-  navbar.style.top = '-98vh';
-});
+
+menuOpen.addEventListener("click", () => { 
+    navbar.style.top = "0"})
+
+menuClose.addEventListener("click", () => { 
+    navbar.style.top = "-98vh"})
+
+
+
+
+
+
